8d2e04cb4e3f583afd4db2e6ce7409cd0bf7136d
Vahan Nadjarian <vahan@nadjarian.com>
2023-06-13 15:23:12 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, render_as_batch=True)
 
 db.init_app(app)
 
@@ -19,15 +19,42 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+    resp = f''
+    resp += f'<ulID: {animal.id}</ul>'
+    resp += f'<ul>Name: {animal.name}</ul>'
+    resp += f'<ul>Species: {animal.species}</ul>'
+    resp += f'<ul>Zookeeper: {animal.zookeeper.name}</ul>'
+    resp += f'<ul>Enclosure: {animal.enclosure.environment}</ul>'
+
+    return make_response(resp)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    resp = f''
+    resp += f'<ul>ID: {zookeeper.id}</ul>'
+    resp += f'<ul>Name: {zookeeper.name}</ul>'
+    resp += f'<ul>Birthday: {zookeeper.birthday}</ul>'
+
+    for animal in zookeeper.animals:
+        resp += f'<ul>Animal: {animal.name}</ul>'
+
+    return make_response(resp)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    resp = f''
+    resp += f'<ul>ID: {enclosure.id}</ul>'
+    resp += f'<ul>Environment: {enclosure.environment}</ul>'
+    resp += f'<ul>Open to Visitors: {enclosure.open_to_visitors}</ul>'
+
+    for animal in enclosure.animals:
+        resp += f'<ul>Animal: {animal.name}</ul>'
+    
+    return make_response(resp)
 
 
 if __name__ == '__main__':
